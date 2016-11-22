ad0dad25ff4ede7948be1faa1935366be41be0bd
valdi1212 <valdi1212@gmail.com>
2016-11-22 16:28:57 +0000

    Added data to DB

--- db_init_data.sql
+++ db_init_data.sql
@@ -15,3 +15,19 @@ INSERT INTO genres (name) VALUES ('Sports');
 -- Games
 
 -- Departments
+INSERT INTO departments (dep_id, name, description) VALUES
+  (1, 'StandardUsers', ''),
+  (2, 'Reviewers', ''),
+  (3, 'Programmers', ''),
+  (4, 'Bossman', '');
+
+-- Users
+INSERT INTO Users (user_id, dep_id, name, email, username, access_level, joined, loggedin, title) VALUES
+  (1, 4, 'Ólafur Jón Valgeirsson', 'Olafurjonb@hotmail.com', 'OlaVal', 4, '21.11.2016', 0, 'CEO'),
+  (2, 4, 'Valdimar Gunnarsson', 'valdi1212@gmail.com', 'valdi1212', 4, '21.11.2016', 0, 'CEO'),
+  (3, 3, 'Andrea Klara Hauksdóttir', 'Andreahar@gmail.com', 'AndHau', 3, '21.11.2016', 0, 'Programmer'),
+  (4, 3, 'Guðmundur Steindórsson', 'Gummisteinn@gmail.com', 'GudSte', 3, '21.11.2016', 0, 'Programmer'),
+  (5, 2, 'Halldor V. Jónsson', 'HalliVal@hotmail.com', 'HalJon', 2, '21.11.2016', 0, 'Reviewer'),
+  (6, 2, 'Harpa Hjaltested', 'Harpa@hotmail.com', 'Harhja', 2, '21.11.2016', 0, 'Reviewer'),
+  (7, 1, 'Hrannar Arason', 'Hrannaara@hotmail.com', 'HraAra', 0, '21.11.2016', 0, 'StandardUser'),
+  (8, 1, 'Ingibjörg J. Guðlaugsdóttir', 'ingagugga@hotmail.com', 'IngGud', 0, '21.11.2016', 0, 'StandardUser');
