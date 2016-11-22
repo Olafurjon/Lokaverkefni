354c896a0c09c41880d74a07db2343525136a84a
valdi1212 <valdi1212@gmail.com>
2016-11-22 15:45:06 +0000

    adding content to database

---
+++ db_init_data.sql
@@ -0,0 +1,17 @@
+-- Genres
+INSERT INTO genres (name) VALUES ('4X');
+INSERT INTO genres (name) VALUES ('Action');
+INSERT INTO genres (name) VALUES ('Adventure');
+INSERT INTO genres (name) VALUES ('Arcade');
+INSERT INTO genres (name) VALUES ('Fantasy');
+INSERT INTO genres (name) VALUES ('RPG');
+INSERT INTO genres (name) VALUES ('Sandbox');
+INSERT INTO genres (name) VALUES ('Simulation');
+INSERT INTO genres (name) VALUES ('Survival');
+INSERT INTO genres (name) VALUES ('Stealth');
+INSERT INTO genres (name) VALUES ('Strategy');
+INSERT INTO genres (name) VALUES ('Sports');
+
+-- Games
+
+-- Users
